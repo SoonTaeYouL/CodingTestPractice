1ff6897ba75a4b894e73f287302b70392879951f
SoonTaeYouL <116995087+SoonTaeYouL@users.noreply.github.com>
2023-11-09 23:52:09 +0900

    [level unrated] Title: 로그인 성공?, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120883. 로그인 성공？/로그인 성공？.py
@@ -0,0 +1,12 @@
+def solution(id_pw, db):
+    answer = ''
+    for i in db:
+        if id_pw[0] == i[0] and id_pw[1] == i[1]:
+            answer = "login"
+            break
+        elif id_pw[0] == i[0] and id_pw[1] != i[1]:
+            answer = "wrong pw"
+            break
+        elif id_pw[0] != i[0]:
+            answer = "fail"
+    return answer
