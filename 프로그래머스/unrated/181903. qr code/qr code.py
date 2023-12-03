937165da7a8676ac07daa8efbc8734c3b23b4bd6
SoonTaeYouL <116995087+SoonTaeYouL@users.noreply.github.com>
2023-12-03 14:48:19 +0900

    [level unrated] Title: qr code, Time: 0.00 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/unrated/181903. qr code/qr code.py
+++ 프로그래머스/unrated/181903. qr code/qr code.py
@@ -1,6 +1,5 @@
 def solution(q, r, code):
-    answer = ''
-    for i in range(len(code)):
-        if i%q==r:
-            answer+=code[i]
-    return answer
+    return code[r::q]
+
+# index를 q로 나눈 나머지가 r이면 index=q*무언가 + r
+# r부터 시작해서 q만큼 반복
