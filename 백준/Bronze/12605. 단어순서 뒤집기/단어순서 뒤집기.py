66c9e1cdf18aa5b92106f3b82a1ca63932021ff8
SoonTaeYouL <116995087+SoonTaeYouL@users.noreply.github.com>
2024-02-16 18:24:24 +0900

    [Bronze II] Title: 단어순서 뒤집기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/12605. 단어순서 뒤집기/단어순서 뒤집기.py
+++ 백준/Bronze/12605. 단어순서 뒤집기/단어순서 뒤집기.py
@@ -1,15 +1,2 @@
-# n = int(input())
-# sen = [input().split() for _ in range(n)]
-# new = []
-# for i in sen:
-#     list = []
-#     for j in range(len(i)):
-#         list.append(i[len(i)-j-1])
-#     new.append(list)
-# for x in range(n):
-#     print(f"Case #{x+1}:",*new[x])
-
-
-
 for i in range(int(input())):
-    print(f"Case #{i+1}:",*input().split()[::-1])
+    print(f'Case #{i+1}:',*input().split()[::-1])
