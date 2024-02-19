700b730714505f9f1be129f57eb4a613069e4e64
SoonTaeYouL <116995087+SoonTaeYouL@users.noreply.github.com>
2024-02-19 22:23:47 +0900

    [Silver IV] Title: 덱, Time: 56 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/10866. 덱/덱.py
@@ -0,0 +1,26 @@
+import sys
+
+n = int(sys.stdin.readline())
+q=[]
+for i in range(n):
+    s = sys.stdin.readline().split()
+    if s[0] == 'push_front': q.insert(0,int(s[1]))
+    elif s[0] == 'push_back': q.append(int(s[1]))
+    elif s[0] == 'pop_front':
+        if len(q)==0: print(-1)
+        else:
+            print(q.pop(0))
+    elif s[0] == 'pop_back':
+        if len(q)==0: print(-1)
+        else:
+            print(q.pop())
+    elif s[0] == 'size': print(len(q))
+    elif s[0] == 'empty':
+        if len(q)==0: print(1)
+        else: print(0)
+    elif s[0] == 'front':
+        if len(q)==0: print(-1)
+        else: print(q[0])
+    elif s[0] == 'back':
+        if len(q)==0: print(-1)
+        else: print(q[-1])
