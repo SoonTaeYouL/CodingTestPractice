06d18a6e35504104c7ac4e1a1ef99ca465e8f722
SoonTaeYouL <116995087+SoonTaeYouL@users.noreply.github.com>
2024-03-15 18:56:20 +0900

    [Bronze I] Title: 부녀회장이 될테야, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.py
+++ 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.py
@@ -1,20 +1,12 @@
 import sys
 
-# def ho(a,b):
-#     hap=0
-#     if a==0:
-#         return b
-#     else:
-#         for i in range(1,b+1):
-#             hap+=ho(a-1,i)
-#         return hap
-
 n=int(sys.stdin.readline())
+
 for _ in range(n):
     a=int(sys.stdin.readline())
     b=int(sys.stdin.readline())
-    l=[i for i in range(1,b+1)]
-    for i in range(a):
-        for j in range(1,b):
-            l[j]+=l[j-1]
+    l=[t+1 for t in range(b)]
+    for _ in range(a):
+        for i in range(1,b):
+            l[i]+=l[i-1]
     print(l[-1])
