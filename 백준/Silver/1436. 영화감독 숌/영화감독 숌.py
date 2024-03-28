3c12f5d66a76cfbf1859a66632d34bf0bb8fbb63
SoonTaeYouL <116995087+SoonTaeYouL@users.noreply.github.com>
2024-03-28 23:11:18 +0900

    [Silver V] Title: 영화감독 숌, Time: 636 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.py
@@ -0,0 +1,14 @@
+import sys
+
+input=sys.stdin.readline
+n=int(input())
+s=666
+cnt=0
+
+while True:
+    if '666' in str(s):
+        cnt+=1
+    if cnt==n:
+        print(s)
+        exit()
+    s+=1
