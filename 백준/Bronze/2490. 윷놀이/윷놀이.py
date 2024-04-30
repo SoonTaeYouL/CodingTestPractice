fa3ba250613f5d2307607f2505afc86dbe95ce2e
SoonTaeYouL <116995087+SoonTaeYouL@users.noreply.github.com>
2024-04-30 22:01:05 +0900

    [Bronze III] Title: 윷놀이, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2490. 윷놀이/윷놀이.py
@@ -0,0 +1,10 @@
+import sys
+
+input = sys.stdin.readline
+for _ in range(3):
+    l=list(map(int,input().split()))
+    if l.count(0)==1: print('A')
+    elif l.count(0)==2: print('B')
+    elif l.count(0)==3: print('C')
+    elif l.count(0)==4: print('D')
+    else: print('E')
