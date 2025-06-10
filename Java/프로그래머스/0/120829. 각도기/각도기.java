7b2ce4965425887c7aa1cd1b8defbbabe98dcf46
SoonTaeYouL <116995087+SoonTaeYouL@users.noreply.github.com>
2025-06-10 11:04:22 +0900

    [level 0] Title: 각도기, Time: 0.02 ms, Memory: 82.8 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/120829. 각도기/각도기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int angle) {
+        switch(angle) {
+            case 90:
+                return 2;
+            case 180:
+                return 4;
+            default:
+                if (angle<90) {
+                    return 1;
+                } else {
+                    return 3;
+                }
+        }
+    }
+}
